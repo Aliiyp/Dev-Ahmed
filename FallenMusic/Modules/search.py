794fb04566aeb16f8dd24ef59e8ae126a8a5813e
Aliiyp <157922994+Aliiyp@users.noreply.github.com>
2024-01-29 05:22:43 +0300

    search.py

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -35,7 +35,7 @@ async def ytsearch(_, message: Message):
         pass
     try:
         if len(message.command) < 2:
-            return await message.reply_text("⎊ اكتب اللى عايز تبحث عنه 🎧")
+            return await message.reply_text("⎊ اكتب اليَ تريد تبحث عنه 🎧")
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("⎊ جارٍ البحث...")
         results = YoutubeSearch(query, max_results=4).to_dict()
